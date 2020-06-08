2e3e9756546e6800892039e5cde811381d72ebac
Tou Toua Yang <57574477+gittyang@users.noreply.github.com>
2020-06-08 17:03:58 -0500

    10th commit: created author&category.php - queried objects + display, refactor with blog-loop.php, styles

---
+++ themes/GymWpTheme/author.php
@@ -0,0 +1,29 @@
+<!-- Author Page -->
+
+<?php get_header(); ?>
+
+<p>hello from author.php</p>
+
+<main class="container page section">
+
+  <?php 
+  
+  $author = get_queried_object(); 
+
+  // -- How to look inside $author object
+  // echo "<pre>";
+  // var_dump($author);
+  // echo "</pre>";
+
+  ?>
+
+  <!-- obtain display name inside $author object -->
+  <h2 class="text-center text-primary">Author: <?php echo $author->data->display_name; ?></h2>
+  <p class="text-center"><?php echo get_the_author_meta('description', $author->data->ID); ?></p>
+
+  <?php get_template_part('template-parts/blog', 'loop'); ?>
+  
+</main>
+
+<?php get_footer(); ?>
+
---
+++ themes/GymWpTheme/category.php
@@ -0,0 +1,23 @@
+<!-- Category Page -->
+
+<?php get_header(); ?>
+
+<p>hello from home.php</p>
+
+<main class="container page section">
+
+  <?php
+    $category = get_queried_object();
+  ?>
+
+  <h2 class="text-center text-primary">Category: <?php echo $category->name; ?></h2>
+  <div class="text-center">
+    <?php echo category_description($category->ID) ?>
+  </div>
+
+  <?php get_template_part('template-parts/blog', 'loop'); ?>
+
+</main>
+
+<?php get_footer(); ?>
+
